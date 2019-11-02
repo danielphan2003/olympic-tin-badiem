51528309b59e06938027def0c79bb1e12607e42e
Daniel Phan <danielphan.2003@gmail.com>
2019-11-02 10:14:56 +0700

    Problem MINK

---
+++ MINK/MINK.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int N, k;
+    deque<int> deq;
+
+    cin >> N >> k;
+    vector<int> A(N+1, 0);
+
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 1; i <= N; i++) {
+        while (!deq.empty() && a[i] )
+        deq.
+    }
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (; T >= 0; T--) {
+        solve();
+    }
+}
