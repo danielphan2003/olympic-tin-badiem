bf7996ebb69799f632a3e201b46a7189ef2d273e
Daniel Phan <danielphan.2003@gmail.com>
2019-11-15 22:33:35 +0700

    first impl of merge sort

---
+++ sort/merge_sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge_sort(vector<int> &x) {
+    int first, second;
+    
+}
+
+int main() {
+    vector<int> arr {1, 2, 9, 1, 48, 32, 61, 13, 12, 17};
+
+    
+    cin.get();
+    return 0;
+}
